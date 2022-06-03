eaf93bf2accc94968e1c929585e5b7d5b5e204fb
roilopezz <roilopez946@gmail.com>
2022-06-04 02:35:31 +0300

    upp

--- components/2.header/header.tsx
+++ components/2.header/header.tsx
@@ -35,7 +35,7 @@ function Header() {
 
       <div className="flex flex-col text-center justify-center items-center w-full sm:mb-[50%]  lg:mb-[10%] mt-56">
         <div className="absolute top-[-50px] left-[637px] h-[380px] w-[315px] lg:block mobile:hidden">
-          <Image alt="img" src={imgSeven} alt="" />
+          <Image alt="img" src={imgSeven} />
         </div>
 
         {lng.map((content: any) => (
